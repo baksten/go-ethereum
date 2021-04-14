50790097353a20cd2b2c7e9861185a3f75614b14
Thomas <baksten@protonmail.com>
2021-04-14 18:45:36 +0200

    checkpoint hash mismatch

--- params/config.go
+++ params/config.go
@@ -96,7 +96,23 @@ var (
 		Threshold: 2,
 	}
 
-	CheapethChainConfig = MainnetChainConfig
+	CheapethChainConfig = &ChainConfig{
+		ChainID:             big.NewInt(1),
+		HomesteadBlock:      big.NewInt(1_150_000),
+		DAOForkBlock:        big.NewInt(1_920_000),
+		DAOForkSupport:      true,
+		EIP150Block:         big.NewInt(2_463_000),
+		EIP150Hash:          common.HexToHash("0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0"),
+		EIP155Block:         big.NewInt(2_675_000),
+		EIP158Block:         big.NewInt(2_675_000),
+		ByzantiumBlock:      big.NewInt(4_370_000),
+		ConstantinopleBlock: big.NewInt(7_280_000),
+		PetersburgBlock:     big.NewInt(7_280_000),
+		IstanbulBlock:       big.NewInt(9_069_000),
+		MuirGlacierBlock:    big.NewInt(9_200_000),
+		CheapethForkBlock:   big.NewInt(11_818_960),
+		Ethash:              new(EthashConfig),
+	}
 
 	// RopstenChainConfig contains the chain parameters to run a node on the Ropsten test network.
 	RopstenChainConfig = &ChainConfig{
